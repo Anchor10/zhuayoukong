67387d724ae2fed0ffda1e164021558d0e10e65e
anchor10 <1129713332@qq.com>
2018-10-16 18:27:59 +0800

    add topic新.html

--- static/js/topic.js
+++ static/js/topic.js
@@ -2,3 +2,18 @@
 $(function(){
     $("#t_zhuanti").addClass("curr");
 })
+
+$(function(){
+    var num = sessionStorage.getItem('num');
+    console.log(num)
+    if(num){
+        $(".snav a").eq(num).addClass("cur").siblings().removeClass("cur");
+        $(".topic-list").eq(num).show().siblings(".topic-list").hide();
+    }
+    $(".snav").on("click","a",function(){
+        $(this).addClass("cur").siblings().removeClass("cur");
+        $(".topic-list").eq($(this).index()).show().siblings(".topic-list").hide();
+        sessionStorage.setItem('num',($(this).index()))
+        
+    })
+})
